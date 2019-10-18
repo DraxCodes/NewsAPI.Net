73c081e38f6d3a838809641cd8b6c76d82b34950
Joel Parkinson <joelparkinson@live.co.uk>
2019-10-18 13:07:23 +0100

    Add XML documentation for TopHeadlinesRequest entity.

--- src/NewsAPI/Entities/TopHeadlinesRequest.cs
+++ src/NewsAPI/Entities/TopHeadlinesRequest.cs
@@ -8,23 +8,43 @@ namespace NewsAPI.Entities
         public Country Country { get; private set; }
         public NewsCategory Category { get; private set; }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(TopHeadlinesRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
         public TopHeadlinesRequest(string query)
         {
             Query = query;
         }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(TopHeadlinesRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <param name="country">The country you wish to find results for. Example: <see cref="Country.Australia"/>.</param>
         public TopHeadlinesRequest(string query, Country country)
         {
             Query = query;
             Country = country;
         }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(TopHeadlinesRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <param name="category">The category you wish to search. Example: <see cref="NewsCategory.Technology"/>.</param>
         public TopHeadlinesRequest(string query, NewsCategory category)
         {
             Query = query;
             Category = category;
         }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(TopHeadlinesRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <param name="country">The country you wish to find results for. Example: <see cref="Country.Australia"/>.</param>
+        /// <param name="category">The category you wish to search. Example: <see cref="NewsCategory.Technology"/>.</param>
         public TopHeadlinesRequest(string query, Country country, NewsCategory category)
         {
             Query = query;
