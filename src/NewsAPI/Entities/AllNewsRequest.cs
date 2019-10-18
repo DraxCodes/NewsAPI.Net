4923d0b4019fc3305e04698f1be20364d908d9ed
Joel Parkinson <joelparkinson@live.co.uk>
2019-10-18 13:03:32 +0100

    Add XML Documentation for AllNewsRequest

--- src/NewsAPI/Entities/AllNewsRequest.cs
+++ src/NewsAPI/Entities/AllNewsRequest.cs
@@ -3,19 +3,48 @@ using System;
 
 namespace NewsAPI.Entities
 {
+    /// <summary>
+    /// An all news request entity.
+    /// </summary>
     public class AllNewsRequest : IRequest
     {
+        /// <summary>
+        /// The search query.
+        /// </summary>
         public string Query { get; private set; }
+
+        /// <summary>
+        /// The oldest date an article can have.
+        /// </summary>
         public DateTime? FromDate { get; private set; }
+
+        /// <summary>
+        /// The newest date an article can have.
+        /// </summary>
         public DateTime? ToDate { get; private set; }
+
+        /// <summary>
+        /// How you want to sort the results.
+        /// </summary>
         public SortType SortType { get; private set; }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(AllNewsRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <param name="sortType">How you want to sort the results.</param>
         public AllNewsRequest(string query, SortType sortType)
         {
             Query = query;
             SortType = sortType;
         }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(AllNewsRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <param name="sortType">How you want to sort the results.</param>
+        /// <param name="fromDate">The oldest date an article can have.</param>
         public AllNewsRequest(string query, SortType sortType, DateTime fromDate)
         {
             Query = query;
@@ -23,6 +52,13 @@ namespace NewsAPI.Entities
             FromDate = fromDate;
         }
 
+        /// <summary>
+        /// Creates an all news request entity which is used in <see cref="NewsClient.FetchNewsAsync(AllNewsRequest)"/>.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <param name="sortType">How you want to sort the results.</param>
+        /// <param name="fromDate">The oldest date an article can have.</param>
+        /// <param name="toDate">The newest date an article can have.</param>
         public AllNewsRequest(string query, SortType sortType, DateTime fromDate, DateTime toDate)
         {
             Query = query;
